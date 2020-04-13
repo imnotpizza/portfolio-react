8b78d68a2e58ee497849261835e440fe7daca6c5
name1 <bobin6972@gmail.com>
2020-04-13 20:47:17 +0900

    04/13 추가커밋

--- movie-search-app/src/constants.js
+++ movie-search-app/src/constants.js
@@ -1,6 +1,6 @@
 
-export const BASE_URL = "http://localhost:3000/api";
-//export const BASE_URL="http://ec2-13-209-66-146.ap-northeast-2.compute.amazonaws.com:3000/api";
+//export const BASE_URL = "http://localhost:3000/api";
+export const BASE_URL="http://ec2-13-209-66-146.ap-northeast-2.compute.amazonaws.com:3000/api";
 
 export const INTRO_IMG="https://myportfolio-resources.s3.ap-northeast-2.amazonaws.com/intro-background.jpg";
 
